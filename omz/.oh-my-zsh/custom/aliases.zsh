415a50c9d03657ca8a34d5781e17f5837765db0e
Mike <16027530+mayknxyz@users.noreply.github.com>
2025-11-15 20:24:13 +0800

    feat: Add ghostty config + Install powerlevel10k zsh theme + Add prefix to custom aliases (#17)

--- omz/.oh-my-zsh/custom/aliases.zsh
+++ omz/.oh-my-zsh/custom/aliases.zsh
@@ -1,13 +1,30 @@
 # Run command lista to open a custom note-taking markdown 
-alias lista='nvim $(fd lista.md)'
+alias my-lista='nvim $(fd lista-tldr.md)'
+alias my-lista-journal='nvim $(fd lista-journal.md)'
+alias my-lista-ideas='nvim $(fd lista-ideas.md)'
+alias my-lista-projects='nvim $(fd lista-projects.md)'
+alias my-lista-wiki='z ~/.lista.wiki && la'
+#Edit custom aliases
+alias my-omz-alias='nvim .oh-my-zsh/custom/aliases.zsh'
+
+# Search for a file, then open with a specific application 
+alias my-fzf-nvim='nvim $(fzf)'
+alias my-fzf-bat='bat $(fzf)'
+alias my-fzf-cp='cp $(fzf)'
+alias my-fzf-mv='mv $(fzf)'
+
+# Run 'git worktree' command
+alias gitw='git worktree'
 
 ## Custom alias to replace copydir functionality
 # Copies the current directory path to the system clipboard
-# ccwd = Copy Current Working Directory
-alias ccwd='pwd | tr -d "\n" | xclip -selection clipboard'
 
-# Currently using omz eza plugin instead
-# eza aliases for 'ls' replacement
-# alias ls='eza --icons --group-directories-first'
-# alias ll='eza -lag --icons --group-directories-first'
-# alias la='eza -a --icons --group-directories-first'
+# Function to Copy Current Working Directory to Clipboard
+my-cp-cwd () {
+  # The semicolon (;) is necessary here if you want the confirmation echo
+  pwd | tr -d '\n' | wl-copy
+  echo "Copied $(pwd) to clipboard."
+}
+
+# Install my-kit
+alias my-mykit-install='bash /home/mayknxyz/Dev/Github/MNXYZ/mnxyz-5-my-kit/script_install.sh'
